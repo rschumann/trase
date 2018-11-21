0830f358ba3fa000dfc1120cbf9cec69800458f3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-11-21 11:22:49 +0000

    Added download only endpoints for serious stress tests

--- load-testing/load-test.sh
+++ load-testing/load-test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-siege -c 50 -t 5M -d 5 -i -f server-urls.txt
-#siege -c 50 -t 5M -d 5 -i -f test-endpoint.txt
+# siege -c 20 -t 5M -d 5 -i -f apache-urls.txt
+# siege -c 30 -t 5M -d 5 -i -f apache-urls.txt
+# siege -c 20 -t 1M -d 5 -i -f apache-download-urls.txt
+
+# siege -c 20 -t 5M -d 5 -i -f server-urls.txt
+# siege -c 30 -t 5M -d 5 -i -f server-urls.txt
+# siege -c 40 -t 5M -d 5 -i -f server-urls.txt
+siege -c 20 -t 2M -d 5 -i -f server-download-urls.txt
+
+# siege -c 50 -t 5M -d 5 -i -f test-endpoint.txt
